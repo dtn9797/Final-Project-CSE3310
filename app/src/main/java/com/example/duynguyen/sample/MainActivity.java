a72623e6bb506358412b63f96ab9ededc3448dc8
Duy Nguyen <dtn9797@gmail.com>
2018-10-26 17:39:49 -0500

    First Commit

--- app/src/main/java/com/example/duynguyen/sample/MainActivity.java
+++ app/src/main/java/com/example/duynguyen/sample/MainActivity.java
@@ -1,70 +1,37 @@
 package com.example.duynguyen.sample;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
 
 public class MainActivity extends AppCompatActivity {
 
-    private DatabaseReference mDatabase;
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Initialize database
-        mDatabase = FirebaseDatabase.getInstance().getReference();
 
-        //Add data in database
-        Button addBtn = (Button) findViewById(R.id.add_btn);
-        addBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                writeToDatabase("test", "Test");
-            }
-        });
-
-        //Read data in database
-        Button readBtn = (Button) findViewById(R.id.read_btn);
-        readBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                readDatabase("test");
-            }
-        });
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
 
-    private void readDatabase(final String child) {
-        mDatabase.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                DataSnapshot readData = dataSnapshot.child(child);
-                Toast.makeText(getApplicationContext(), "Read data is " + readData.toString(), Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Toast.makeText(getApplicationContext(), "Failed to read data", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
-
-    private void writeToDatabase(String child, String text) {
-
-        mDatabase.child(child).setValue(text);
-    }
-
 }
+
