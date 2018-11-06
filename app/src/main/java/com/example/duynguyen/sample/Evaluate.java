0f61fbfa524a0420304746cd1f5387890ab9bb6a
poh <kitpham@gmail.com>
2018-11-06 16:28:35 -0600

    added evaluation activity to main page

---
+++ app/src/main/java/com/example/duynguyen/sample/Evaluate.java
@@ -0,0 +1,13 @@
+package com.example.duynguyen.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Evaluate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_evaluate);
+    }
+}
