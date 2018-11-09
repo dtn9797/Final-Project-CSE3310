382d4de4efe1ebb576137cd430f423325c19d06e
Duy Nguyen <dtn9797@gmail.com>
2018-11-08 21:34:22 -0600

    added Login activity

--- app/src/main/java/com/example/duynguyen/sample/LoginActivity.java
+++ app/src/main/java/com/example/duynguyen/sample/LoginActivity.java
@@ -1,4 +1,120 @@
 package com.example.duynguyen.sample;
 
-public class LoginActivity {
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.content.res.XmlResourceParser;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static EditText emailid, password;
+    private static Button loginButton;
+    private static TextView forgotPassword, signUp;
+    private static CheckBox show_hide_password;
+    private static LinearLayout loginLayout;
+    private static Animation shakeAnimation;
+    private static FragmentManager fragmentManager;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        initViews();
+        setListeners();
+    }
+
+    private void initViews() {
+
+        emailid = (EditText) findViewById(R.id.login_emailid);
+        password = (EditText)findViewById(R.id.login_password);
+        loginButton = (Button) findViewById(R.id.loginBtn);
+        forgotPassword = (TextView) findViewById(R.id.forgot_password);
+        signUp = (TextView) findViewById(R.id.createAccount);
+        show_hide_password = (CheckBox)
+                findViewById(R.id.show_hide_password);
+        loginLayout = (LinearLayout) findViewById(R.id.login_layout);
+
+        // Load ShakeAnimation
+        shakeAnimation = AnimationUtils.loadAnimation(this,
+                R.anim.shake);
+
+        // Setting text selector over textviews
+        XmlResourceParser xrp = getResources().getXml(R.drawable.text_selector);
+        try {
+            ColorStateList csl = ColorStateList.createFromXml(getResources(),
+                    xrp);
+
+            forgotPassword.setTextColor(csl);
+            show_hide_password.setTextColor(csl);
+            signUp.setTextColor(csl);
+        } catch (Exception e) {
+        }
+    }
+
+    private void setListeners() {
+        loginButton.setOnClickListener(this);
+        forgotPassword.setOnClickListener(this);
+        signUp.setOnClickListener(this);
+
+        // Set check listener over checkbox for showing and hiding password
+        show_hide_password
+                .setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+                    @Override
+                    public void onCheckedChanged(CompoundButton button,
+                                                 boolean isChecked) {
+
+                        // If it is checkec then show password else hide
+                        // password
+                        if (isChecked) {
+
+                            show_hide_password.setText(R.string.hide_pwd);// change
+                            // checkbox
+                            // text
+
+                            password.setInputType(InputType.TYPE_CLASS_TEXT);
+                            password.setTransformationMethod(HideReturnsTransformationMethod
+                                    .getInstance());// show password
+                        } else {
+                            show_hide_password.setText(R.string.show_pwd);// change
+                            // checkbox
+                            // text
+
+                            password.setInputType(InputType.TYPE_CLASS_TEXT
+                                    | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                            password.setTransformationMethod(PasswordTransformationMethod
+                                    .getInstance());// hide password
+
+                        }
+
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.forgot_password:
+
+                // Replace forgot password fragment with animation
+//                Intent forgotIntent = new Intent(this,)
+                break;
+        }
+    }
 }
